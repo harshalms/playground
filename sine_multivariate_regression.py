1b8aaefb40e8a61d91e51b28bcd489be1e898454
Harshal <harshalshedolkar.sy@gmail.com>
2020-04-26 19:06:31 +0530

    double variable regression

--- sine_multivariate_regression.py
+++ sine_multivariate_regression.py
@@ -12,17 +12,17 @@ plt.scatter(x, y)
 w0 = 0
 w1 = 0
 w2 = 0
-alpha = 0.01
+alpha = 0.001
 # cost = np.sum(w0 + w1*np.sin(x) - y, axis=0)/(2*m)
 
-for epochs in range(1000):
+for epochs in range(10000):
     cost = np.sum(w0 + w1*x + w2*x*x - y, axis=0)/(2*m)
     print('cost: ', cost)
     w0 = w0 - np.sum(w0 + w1*x + w2*x*x - y, axis=0)*(alpha/m)
     w1 = w1 - np.sum((w0 + w1*x + w2*x*x - y)*x, axis=0)*(alpha/m)
-    w2 = w2 - np.sum(2*(w0 + w1*x + w2*x*x - y)*x*x, axis=0)*(alpha/m)
+    w2 = w2 - np.sum((w0 + w1*x + w2*x*x - y)*x*x, axis=0)*(alpha/m)
 
 y_hat = w0 + w1*x + w2*x*x
 plt.plot(x, y_hat)
 plt.show()
-# print(w0, w1)
+print(w0, w1, w2)
